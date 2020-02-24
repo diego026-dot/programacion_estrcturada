5b85f7e12adc8a30ef500f17930332d270559435
diego026-dot <60374205+diego026-dot@users.noreply.github.com>
2020-02-24 00:54:01 -0600

    Add files via upload

---
+++ Practica 3.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	char c;
+	
+	printf("Ingrese un caracter:\n");
+	c = getchar();
+	int a = (int) c;
+	if(48<= a&&a <= 57)
+		printf("Numero ingresado\n");
+	else
+		if(65<= a&&a <=90)
+			printf("Mayuscula ingresada\n");
+		else
+			if(97<= a&&a <=122)
+				printf("Minuscula Ingresada");
+			else
+				printf("Simbolo ingresado\n");
+	
+	system("pause");
+	return 0;						
+	
+}
